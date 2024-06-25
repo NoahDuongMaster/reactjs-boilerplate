43db927232af736db599e2c866066156da49b135
truongdn <truongdn@fabbi.com.vn>
2024-06-25 10:13:49 +0700

    refactor: remove million lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { ValidateEnv } from '@julr/vite-plugin-validate-env';
-import MillionLint from '@million/lint';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
+import million from 'million/compiler';
 import { defineConfig } from 'vite';
 import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
@@ -9,6 +9,7 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 const _plugins = [
+  million.vite({ auto: true }),
   TanStackRouterVite(),
   react(),
   viteTsconfigPaths(),
@@ -16,7 +17,7 @@ const _plugins = [
   chunkSplitPlugin(),
   nodePolyfills(),
 ];
-_plugins.unshift(MillionLint.vite());
+
 export default defineConfig({
   plugins: _plugins,
 });
