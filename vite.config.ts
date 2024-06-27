2ea55439c0b08facb77af8c8b08623272e5bed39
truongdn <truongdn@fabbi.com.vn>
2024-06-27 11:18:29 +0700

    refactor(dapp): no_issue optimize build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { ValidateEnv } from '@julr/vite-plugin-validate-env';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import million from 'million/compiler';
+import { visualizer } from 'rollup-plugin-visualizer';
+import TurboConsole from 'unplugin-turbo-console/vite';
 import { defineConfig } from 'vite';
+import autoAlias from 'vite-plugin-auto-alias';
 import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
@@ -16,8 +19,35 @@ const _plugins = [
   ValidateEnv(),
   chunkSplitPlugin(),
   nodePolyfills(),
+  TurboConsole(),
+  autoAlias({
+    mode: 'off',
+  }),
+  visualizer({
+    template: 'treemap',
+    open: true,
+    gzipSize: true,
+    brotliSize: true,
+    filename: 'analyzer.html',
+  }),
 ];
 
 export default defineConfig({
   plugins: _plugins,
+  build: {
+    rollupOptions: {
+      output: {
+        sourcemap: false,
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
